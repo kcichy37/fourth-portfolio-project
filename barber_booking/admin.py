b6fb946130e98c443a5963aa178eb3a5eaf124c2
kcichy37 <kcichy37@gmail.com>
2022-11-16 13:13:11 +0000

    Add Booking Admin model

--- barber_booking/admin.py
+++ barber_booking/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Booking
 
-# Register your models here.
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'phone', 'date', 'time', 'barber',
+                    'created_on')
+    search_field = ('full_name', 'barber')
+    list_filter = ('barber', 'date')
--- barber_booking/models.py
+++ barber_booking/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Booking(models.Model):
+    full_name = models.CharField(max_length=200, unique=True)
+    email = models.EmailField()
+    phone = models.CharField(max_length=50)
+    date = models.DateField()
+    time = models.TimeField()
+    barber = models.CharField(max_length=100)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["created_on"]
