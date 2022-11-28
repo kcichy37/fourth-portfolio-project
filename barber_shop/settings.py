c4f424bc9f1fc4eee043e1809064acb1af640cef
kcichy37 <kcichy37@gmail.com>
2022-11-28 11:42:28 +0000

    Change settings

--- barber_shop/settings.py
+++ barber_shop/settings.py
@@ -33,6 +33,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+X_FRAME_OPTION = 'SAMEORIGIN'
+
 ALLOWED_HOSTS = ["fourth-portfolio-project.herokuapp.com", "localhost"]
 
 ACCOUNT_EMAIL_VERIFICATION = "none"
@@ -119,20 +121,16 @@ if "test" in sys.argv:
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        "NAME": """django.contrib.auth.password_validation.
-                UserAttributeSimilarityValidator""",
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        "NAME": """django.contrib.auth.password_validation.
-                MinimumLengthValidator""",
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        "NAME": """django.contrib.auth.password_validation.
-                CommonPasswordValidator""",
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        "NAME": """django.contrib.auth.password_validation.
-                NumericPasswordValidator""",
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
